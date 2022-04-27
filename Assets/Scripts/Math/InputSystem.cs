e45ffd3591d3fffbf856a6a5a647d88111d05e8a
public-float-artMajor <ryan.alex.davis@gmail.com>
2022-04-27 16:15:41 -0700

    Testing new input system

--- Assets/Scripts/Math/InputSystem.cs
+++ Assets/Scripts/Math/InputSystem.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class InputSystem : MonoBehaviour
 {
-    private string userInput;
-    //[SerializeField] private Button submitBut;
-
-
-    private string GetChar() {
-        return "";
-    }
-
-    public string GetInput() {
-        return userInput;
+    public void Click() {
+        Debug.Log("Clicked!");
     }
 }
