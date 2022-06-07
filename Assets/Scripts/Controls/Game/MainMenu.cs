8bad206907d45968b315a5a037fd3aa6749b11cb
public-float-artMajor <ryan.alex.davis@gmail.com>
2022-06-06 19:49:11 -0700

    Ref images added, adjusting scrollbox (not yet inline)

--- Assets/Scripts/Controls/Game/MainMenu.cs
+++ Assets/Scripts/Controls/Game/MainMenu.cs
@@ -5,23 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public string firstLevel;
+    public const string LEVEL = "Level3";
     public GameObject optionsScreen;
 
-    // // Start is called before the first frame update
-    // void Start()
-    // {
-
-    // }
-
-    // // Update is called once per frame
-    // void Update()
-    // {
-        
-    // }
-
     public void StartGame() {
-        SceneManager.LoadScene(firstLevel);
+        SceneManager.LoadScene(LEVEL);
     }   
 
     public void OpenOptions() {
